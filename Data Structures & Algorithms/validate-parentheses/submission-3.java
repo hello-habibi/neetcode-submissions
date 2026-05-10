0dc4c3a90ae5086f0094cd690fc52fbe01b43628
Md. Habibur Rahman <hr13338@gmail.com>
2026-05-10 23:15:56 +0600

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+
+       Stack<Character> st = new Stack<>();
+
+       int n = s.length();
+       st.push(s.charAt(0));
+       for(int i = 1 ; i< n ; i++){
+        if(st.isEmpty() && (s.charAt(i) ==')' || s.charAt(i) == '}' || s.charAt(i) == ']')){
+            return false ;
+        }
+        if(!st.isEmpty() && s.charAt(i)==')' && st.peek()=='('){
+            st.pop();
+        } else if(!st.isEmpty() && s.charAt(i)=='}' && st.peek()=='{'){
+            st.pop();
+       }else if(!st.isEmpty() && s.charAt(i)==']' && st.peek()=='['){
+            st.pop();
+       }else{
+        st.push(s.charAt(i));
+       }
+       }
+        return st.isEmpty();
+
+    }
+}
